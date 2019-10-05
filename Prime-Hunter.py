c567787d0997a997bea1075e97d52e05614d9d42
Michael McDougal <michael.93.mcdougal@gmail.com>
2019-10-05 07:53:37 -0400

    Update Prime-Hunter.py
    No retesting factors and only need to check through the square root of the target

--- Prime-Hunter.py
+++ Prime-Hunter.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 prime = [] #2,3,5,7,11
 prime.append(2)
 prime.append(3)
@@ -5,26 +7,45 @@ prime.append(3)
 print(prime[0])
 print(prime[1])
 
+factor = []
+factor.append(0)
+factor.append(0)
+
 test = 5
 while True:
   isPrime = True
 
   i = 1
   while i < len(prime):
-    factor = 0
+    j = i
     loopAgain = True
     while loopAgain:
+      factor[j] += 1
       loopAgain = False
-      factor += 1
-      if test > (prime[i] * factor):
+      if test > (prime[j] * factor[j]):
         loopAgain = True
-      if test == (prime[i] * factor):
+      if test == (prime[j] * factor[j]):
         isPrime = False
         i = len(prime)
+      if prime[j] > sqrt(test):
+        i = len(prime)
+      if loopAgain == False:
+        factor[j] -= 1
     i += 1
   
   if isPrime == True:
     prime.append(test)
-    print(test)
-      
+    factor.append(0)
+
+
+    # 0 for none, 1 for print each prime, 2 for updates on number found
+    output = 1
+
+    if output == 1:
+      print(test)
+    elif output == 2:
+      scale = 1000
+      if int(len(prime) / scale) == len(prime) / scale:
+        print(str(len(prime)) + " Found!")
+
   test += 2
